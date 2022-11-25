199b07e6474e13bdada59cbd2d6119bdda1fc190
Razin8732 <razinshaikh8732@gmail.com>
2022-11-25 21:13:39 +0530

    formatting

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Image;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
 
 class ImageController extends Controller
 {
@@ -35,7 +33,6 @@ class ImageController extends Controller
         $file = $request->file('image');
         $fileName = time() . '.' . $file->extension(); // created file name with time for uniqueness
         $file->move(public_path('images'), $fileName); // move file to public folder /project_dir/public/image
-
 
         // Create new entry with image name to database.
         $imageObject = new Image();
